c300ce2ca7794038c8c1bb3084de46b8d83dd843
Mannie <carlos.alvarezberrios@gmail.com>
2020-03-14 13:05:16 -0400

    added a few more event listeners

--- js/index.js
+++ js/index.js
@@ -10,24 +10,54 @@ navLinks.forEach(link => link.addEventListener("mouseout", (event) => {
     event.target.style.transform = "scale(1)";
 }));
 
+navLinks.forEach(link => link.addEventListener("click", (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+}));
 
 const body = document.querySelector("body");
+
+window.addEventListener("load", () => {
+    alert("Welcome to Fun Bus!\n\nLet's make your travels fun!!!");
+
+    let name = prompt("What is your name?");
+    name = name[0].toUpperCase() + name.slice(1);
+
+    if(name != "" && name != null) {
+        alert(`Hello ${name}! Let's get do this!`); 
+    }
+    
+});
 
 window.addEventListener("scroll", (event) => {
     const randNum = Math.floor(Math.random() * 9) + 1;
     body.style.background = `#36AF${randNum}${randNum}`;
+    body.style.transition = "all .8s";
 });
 
 const images = document.querySelectorAll("img");
-
+let i = 1;
 window.addEventListener("resize", (event) => {
+    if(i === 1) {
+        i = .5; 
+    } else {
+        i = 1;
+    }
     images.forEach(img => {
-        if(img.style.transform === "scale(1)") {
-            img.style.transform = "scale(.4)";
-        } else {
-            img.style.transform = "scale(1)";
-        }
+
+        img.style.transform = `scale(${i})`;
+        img.style.transition = "all 2s";
         document.querySelector(".main-navigation").style.zIndex = "1";
-        img.style.transition = "all .4s";
     });
+});
+
+const busImg = document.querySelector(".container .intro img");
+
+busImg.addEventListener("dblclick", (event) => {
+    if(busImg.style.filter === " " || busImg.style.filter === "invert(0)") {
+        busImg.style.filter = "invert(1)";
+    } else {
+        busImg.style.filter = "invert(0)";
+    }
+    
 });
